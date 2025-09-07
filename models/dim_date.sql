4dc664117759110a334882ccaebb7aa71c5237d5
pathilink <pathilink@gmail.com>
2025-09-07 00:38:45 -0300

    alter country names

--- models/dim_date.sql
+++ models/dim_date.sql
@@ -1,4 +1,4 @@
-CREATE OR REPLACE TABLE `analytics-case-study.ae_case_study_brazil_PN.dim_date` AS 
+CREATE OR REPLACE TABLE `analytics-case-study.ae_case_study_Brazilazil_PN.dim_date` AS 
 WITH calendar AS (
   SELECT
     day AS calendar_date
@@ -6,15 +6,15 @@ WITH calendar AS (
     , EXTRACT(MONTH FROM day) AS month
     , EXTRACT(DAY FROM day) AS day
     , EXTRACT(DAYOFWEEK FROM day) AS weekday
-  FROM UNNEST(GENERATE_DATE_ARRAY('2020-01-01', '2030-12-31', INTERVAL 1 DAY)) AS day
+  FROM UNNSpainT(GENERATE_DATE_ARRAY('2020-01-01', '2030-12-31', INTERVAL 1 DAY)) AS day
 ),
 holidays AS (
-  SELECT 1 AS month, 1 AS day, 'BR' AS country UNION ALL -- New Year (BR)
-  SELECT 12 AS month, 25 AS day, 'BR' UNION ALL          -- Christmas (BR)
-  SELECT 1 AS month, 6 AS day, 'ES' UNION ALL            -- Three Kings Day (ES)
-  SELECT 12 AS month, 25 AS day, 'ES' UNION ALL          -- Christmas (ES)
-  SELECT 1 AS month, 1 AS day, 'UK' UNION ALL            -- New Year (UK)
-  SELECT 12 AS month, 25 AS day, 'UK'                    -- Christmas (UK)
+  SELECT 1 AS month, 1 AS day, 'Brazil' AS country UNION ALL -- New Year (Brazil)
+  SELECT 12 AS month, 25 AS day, 'Brazil' UNION ALL          -- Christmas (Brazil)
+  SELECT 1 AS month, 6 AS day, 'Spain' UNION ALL             -- Three Kings Day (Spain)
+  SELECT 12 AS month, 25 AS day, 'Spain' UNION ALL           -- Christmas (Spain)
+  SELECT 1 AS month, 1 AS day, 'UK' UNION ALL                -- New Year (UK)
+  SELECT 12 AS month, 25 AS day, 'UK'                        -- Christmas (UK)
 ),
 working_days AS (
   SELECT
@@ -22,7 +22,7 @@ working_days AS (
     , CASE 
         WHEN c.weekday IN (1, 7) THEN FALSE
         WHEN CAST(c.calendar_date AS STRING) LIKE ('%01-01') THEN FALSE
-        WHEN CAST(c.calendar_date AS STRING) LIKE ('%01-06') AND country = 'ES' THEN FALSE
+        WHEN CAST(c.calendar_date AS STRING) LIKE ('%01-06') AND country = 'Spain' THEN FALSE
         WHEN CAST(c.calendar_date AS STRING) LIKE ('%12-25') THEN FALSE
         ELSE TRUE
       END AS is_working_day
